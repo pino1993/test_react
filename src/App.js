57d88b732189e67821123ff48dd5b38cade09478
giuseppe <giuseppe.papaleo@mail-bip.com>
2023-04-26 10:24:24 +0200

    tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Product from './Product';
+
+const prodotti= [{
+    id:1,
+  nome: "10$ amazon + 10P",
+  prezzo:50.9,
+  img: 'https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f11902.jpg'
+
+},
+
+{
+    id:2,
+  nome: "155$ amazon + 80P",
+  prezzo:85.7,
+  img: 'https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f14443.jpg'
+
+}];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+          {prodotti.map((prodotto)=>{
+              return <Product key={prodotto.id} {...prodotto}/>
+          })
+          }
+     </section>
     </div>
   );
 }
